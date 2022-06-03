56b31b5bf83618b13f01f26d481613ffd5ecee0c
Konstantinos Alexis <kal@ethinking.de>
2022-06-03 11:43:08 +0200

    resetAllScore

--- src/Game.js
+++ src/Game.js
@@ -5,12 +5,12 @@ import Players from "./Players";
 export default function Game() {
   // const [playerList, setPlayerList] = useState([]);
 
-  const [game,setGame]=useState([])
+  const [game, setGame] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     //ananewnoume to local storage
-    localStorage.setItem('game', game);
-  },[game])
+    localStorage.setItem("game", game);
+  }, [game]);
 
   // useEffect(()=>{
   //   if(localstorage!==empty){
@@ -18,23 +18,34 @@ export default function Game() {
   //   }
   // },[])
 
-  useEffect(()=>{
-    if(localStorage.getItem('game').length!==0){
-      setGame(localStorage.getItem('game'))
+  useEffect(() => {
+    if (localStorage.getItem("game").length !== 0) {
+      setGame(localStorage.getItem("game"));
     }
-  },[])
+  }, []);
 
+  const handleResetGame = () => {
+    setGame([]);
+  };
 
-  const handleResetGame = () =>{
-      setGame([])
-  }
+  const handleResetAllScore = () => {
+    setGame((cur) =>
+      cur.map((item) => {
+        return { name: item.name, score: 0 };
+      })
+    );
+  };
 
   return (
     <>
       {game.length > 0 && <Players setGame={setGame} game={game} />}
       <AddPlayer setGame={setGame} />
-      {game.length > 0 && <div onClick={handleResetGame}>Reset All</div>}
-
+      {game.length > 0 && (
+        <>
+          <div onClick={handleResetAllScore}>Reset All Score</div>
+          <div onClick={handleResetGame}>Reset Game</div>
+        </>
+      )}
     </>
   );
 }
--- src/Player.js
+++ src/Player.js
@@ -1,31 +1,26 @@
 import React from "react";
 
 export default function Player(props) {
-  // const [score, setScore] = useState(0);
   const { player, setGame } = props;
 
   const handleMinus = () => {
-    setGame ((cur)=>
+    setGame((cur) =>
       cur.map((item) => {
-          return item.name === player.name? {name:item.name, score:item.score-1}: item;
+        return item.name === player.name
+          ? { name: item.name, score: item.score - 1 }
+          : item;
       })
-  ); 
+    );
   };
 
   const handlePlus = () => {
-    setGame ((cur)=>
-    cur.map((item) => {
-        return item.name === player.name? {name:item.name, score:item.score+1}: item;
-    })
-); 
-  };
-
-  const handleresetScore = () => {
-    setGame ((cur)=>
-    cur.map((item) => {
-        return item.name === player.name? {name:item.name, score:0}: item;
-    })
-); 
+    setGame((cur) =>
+      cur.map((item) => {
+        return item.name === player.name
+          ? { name: item.name, score: item.score + 1 }
+          : item;
+      })
+    );
   };
 
   return (
@@ -34,7 +29,6 @@ export default function Player(props) {
       <div onClick={handlePlus}>+</div>
       <div onClick={handleMinus}>-</div>
       <div>{player.score}</div>
-      <div onClick={handleresetScore}>Reset score</div>
     </div>
   );
 }
